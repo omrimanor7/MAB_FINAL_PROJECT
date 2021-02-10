ad8642a53da9b695a07548bf578c30f13870996d
Omri Manor <omrimanor7@gmail.com>
2021-02-10 13:02:05 +0200

    m update

--- MLinUCB.py
+++ MLinUCB.py
@@ -131,6 +131,7 @@ class MLinUCB:
         visualizer.fit(X_t)  # Fit the data to the visualizer
         N = visualizer.elbow_value_
         self.N = N if N is not None else self.N
+        # TODO
         N = 15
         print("N is", N)
         kmeans = KMeans(n_clusters=N, random_state=0).fit(X_t)
